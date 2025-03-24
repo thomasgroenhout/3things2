51e8d8cea878db7e1f8054f390d0c3f5fda6801a
thomasgroenhout <thomasg3@live.unc.edu>
2025-03-24 08:00:57 -0400

    added files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "3things": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "3thingsac005c01": {
       "AppClientID": "string",
