da4654b3641bafc6378f323de574d2faa62cd88b
thomasgroenhout <thomasg3@live.unc.edu>
2025-03-24 08:05:31 -0400

    edited tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
